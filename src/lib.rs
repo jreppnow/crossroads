6d0c6ffce68008dcff3b8e758e115c42d6513862
Janosch Reppnow <janoschre@gmail.com>
2022-12-20 17:55:41 +0100

    set license to MPL-2.0 for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022 Janosch Reppnow <janoschre+rust@gmail.com>.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use proc_macro::TokenStream;
 use std::collections::VecDeque;
 
